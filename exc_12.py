b257fd370ee2b49ac9dfa04ec2851b93d8ab57dc
Gabriel H <99564238+GabrielH20@users.noreply.github.com>
2023-05-18 14:28:33 -0300

    Add files via upload

---
+++ exc_12.py
@@ -0,0 +1,6 @@
+import random
+numeros_possiveis = [numero for numero in range(1,10)]
+def quadrado_criar(lista_numeros):
+    quadrado_elementos = []
+    for i in range(0,3):
+        elemento_um = random.choice
